eb867b40a5303051541aa25b8bd2541367ba99a6
Isaque Samuel <i27052024@gmail.com>
2024-06-28 22:35:39 +0000

    1.03 - Atualizando meu código repet_text

--- repet_text.py
+++ repet_text.py
@@ -1,3 +1,5 @@
+#Agora vamos solicitar uma string e um número inteiro como entrada. Depois teremos que retornar a string o número de vezes informado pelo usuário
+
 text = input("Digite o texto: ")
 repet = int(input("Digite um valor: "))
 i = 0;
