c2b70e232f54adf4902b733c55d484f331482a90
Isaque Samuel <i27052024@gmail.com>
2024-06-28 22:11:25 +0000

    1.01 - Fazendo o primeiro projeto

---
+++ concat_dados.py
@@ -0,0 +1,8 @@
+#Vamos receber dois valores diferentes do usuário e concatena-los em uma única string
+
+value01 = input("Valor 01: ");
+value02 = input("Valor 02: ");
+
+like = value01 + value02;
+
+print("Concatenando as duas fica: ", like);
