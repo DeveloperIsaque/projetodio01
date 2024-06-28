a82f050f48d5071b9588a4a5c59b3a4f083d10d1
Isaque Samuel <i27052024@gmail.com>
2024-06-28 22:32:56 +0000

    1.02 - Adicionando o outro exercício do mesmo projeto

--- concat_dados.py
+++ concat_dados.py
@@ -1,8 +1,8 @@
 #Vamos receber dois valores diferentes do usuário e concatena-los em uma única string
 
-value01 = input("Valor 01: ");
-value02 = input("Valor 02: ");
+value01 = input("Uma informação: ")
+value02 = input("Outra informação: ")
 
-like = value01 + value02;
+like = value01 + " "+ value02
 
-print("Concatenando as duas fica: ", like);
+print("Concatenando as duas fica: ", like)
---
+++ repet_text.py
@@ -0,0 +1,7 @@
+text = input("Digite o texto: ")
+repet = int(input("Digite um valor: "))
+i = 0;
+
+while i < repet:
+    print("", text)
+    i += 1
